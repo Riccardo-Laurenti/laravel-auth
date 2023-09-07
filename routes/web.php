531e2d993e35d0adfd9c8b26ed28a7c26cc4f751
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-09-07 12:37:18 +0200

    add Model -> Project

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+//Route::prefix('/admin')->name('admin.')->middleware(['auth'])->group(function () {
+//HOME
+//Route::get('/',[]) 
+//});
+
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -28,4 +34,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
